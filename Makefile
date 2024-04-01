a865c40226347527446087c24fe936867e00abe3
darkmage <165251+mikedesu@users.noreply.github.com>
2024-03-31 23:05:40 -0500

    update

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: game
 
 game: main.cpp
-	g++ main.cpp -lSDL2 -lSDL2_image -o game
+	g++ main.cpp -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o game
 
 clean:
 	rm -f game
--- main.cpp
+++ main.cpp
@@ -1,41 +1,77 @@
 #include "SDL_handler.h"
+#include "mPrint.h"
 
+#include <cstdio>
 #include <string>
 
+using std::snprintf;
 using std::string;
 
+#define DEBUG_TEXT_WRAP_LEN 2048
+
+char texture_text[128] = "frame_count: 00000";
+const int target_texture_width = 400;
+const int target_texture_height = 240;
+const int zoom = 4;
+const int window_width = target_texture_width * zoom;
+const int window_height = target_texture_height * zoom;
+int imgFlags = IMG_INIT_PNG;
+int result = -1;
+int w = 0;
+int h = 0;
+int num_clips = 2;
+int frame_count = 0;
+int mWidth = -1;
+int mHeight = -1;
+bool quit = false;
+bool left_is_pressed = false;
+bool right_is_pressed = false;
+bool up_is_pressed = false;
+bool down_is_pressed = false;
+TTF_Font *gFont = nullptr;
+SDL_Event e;
+SDL_Surface *text_surface = nullptr;
+SDL_Color textColor = {255, 255, 255};
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+SDL_Texture *target_texture = nullptr;
+SDL_Texture *skull_sheet_texture = nullptr;
+SDL_Texture *debug_texture = nullptr;
+SDL_Rect skull_clip;
+SDL_Rect skull_dest;
+SDL_Rect target_texture_src;
+SDL_Rect target_texture_dest;
+SDL_Rect debug_texture_src;
+SDL_Rect debug_texture_dest;
+string filepath = "img/skull-sheet.png";
+
+void load_debug_text();
+void load_debug_text() {
+  snprintf(texture_text, 128, "frame_count: %05d", frame_count++);
+  text_surface = TTF_RenderText_Blended_Wrapped(gFont, texture_text, textColor,
+                                                DEBUG_TEXT_WRAP_LEN);
+  if (text_surface == NULL) {
+    mPrint("text_surface == NULL");
+    // printf("textureText = %s\n", textureText);
+    printf("Unable to render text_surface! SDL_ttf Error: %s\n",
+           TTF_GetError());
+  } else {
+    // Create texture from surface pixels
+    debug_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+    if (debug_texture == NULL) {
+      mPrint("mTexture == NULL");
+      printf("Unable to create texture from rendered text! SDL Error: %s\n",
+             SDL_GetError());
+    }
+    // Get image dimensions
+    mWidth = text_surface->w;
+    mHeight = text_surface->h;
+    // Get rid of old surface
+    SDL_FreeSurface(text_surface);
+  }
+}
+
 int main() {
-  const int target_texture_width = 400;
-  const int target_texture_height = 240;
-  const int zoom = 4;
-  const int window_width = target_texture_width * zoom;
-  const int window_height = target_texture_height * zoom;
-
-  SDL_Event e;
-
-  int imgFlags = IMG_INIT_PNG;
-  int result = -1;
-  int w = 0;
-  int h = 0;
-  int num_clips = 2;
-
-  bool quit = false;
-  bool left_is_pressed = false;
-  bool right_is_pressed = false;
-  bool up_is_pressed = false;
-  bool down_is_pressed = false;
-
-  SDL_Window *window = nullptr;
-  SDL_Renderer *renderer = nullptr;
-  SDL_Texture *target_texture = nullptr;
-  SDL_Texture *skull_sheet_texture = nullptr;
-  SDL_Rect skull_clip;
-  SDL_Rect skull_dest;
-  SDL_Rect target_texture_src;
-  SDL_Rect target_texture_dest;
-
-  string filepath = "img/skull-sheet.png";
-
   SDL_Init(SDL_INIT_VIDEO);
   window =
       SDL_CreateWindow("SDL2 Displaying Image", SDL_WINDOWPOS_UNDEFINED,
@@ -43,6 +79,7 @@ int main() {
   renderer = SDL_CreateRenderer(
       window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   result = IMG_Init(imgFlags);
+  result = TTF_Init();
   // init the target texture
   target_texture = SDL_CreateTexture(
       renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET,
@@ -52,6 +89,41 @@ int main() {
     printf("Failed to load texture image!\n");
     return 1;
   }
+
+  gFont = TTF_OpenFont("ttf/hack.ttf", 28);
+  // text_surface = TTF_RenderText_Blended_Wrapped(gFont, texture_text,
+  // textColor,
+  //                                               DEBUG_TEXT_WRAP_LEN);
+  // if (text_surface == NULL) {
+  //   mPrint("text_surface == NULL");
+  //   // printf("textureText = %s\n", textureText);
+  //   printf("Unable to render text_surface! SDL_ttf Error: %s\n",
+  //          TTF_GetError());
+  // } else {
+  //   // Create texture from surface pixels
+  //   debug_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+  //   if (debug_texture == NULL) {
+  //     mPrint("mTexture == NULL");
+  //     printf("Unable to create texture from rendered text! SDL Error: %s\n",
+  //            SDL_GetError());
+  //   }
+  //   // Get image dimensions
+  //   mWidth = text_surface->w;
+  //   mHeight = text_surface->h;
+  //   // Get rid of old surface
+  //   SDL_FreeSurface(text_surface);
+  // }
+
+  load_debug_text();
+
+  debug_texture_src.x = 0;
+  debug_texture_src.y = 0;
+  debug_texture_src.w = mWidth;
+  debug_texture_src.h = mHeight;
+  debug_texture_dest.x = 0;
+  debug_texture_dest.y = 0;
+  debug_texture_dest.w = mWidth;
+  debug_texture_dest.h = mHeight;
   // get the width and height of the texture
   SDL_QueryTexture(skull_sheet_texture, NULL, NULL, &w, &h);
   skull_clip.x = 0;
@@ -121,13 +193,11 @@ int main() {
     } else if (right_is_pressed) {
       skull_dest.x++;
     }
-
     if (up_is_pressed) {
       skull_dest.y--;
     } else if (down_is_pressed) {
       skull_dest.y++;
     }
-
     // render frame
     // clear screen
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
@@ -137,11 +207,18 @@ int main() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, skull_sheet_texture, &skull_clip, &skull_dest);
+    // render debug text
+    // SDL_RenderCopy(renderer, debug_texture, NULL, NULL); // lol
     //  reset the render target
     SDL_SetRenderTarget(renderer, NULL);
     SDL_RenderCopy(renderer, target_texture, &target_texture_src,
                    &target_texture_dest);
+    SDL_RenderCopy(renderer, debug_texture, &debug_texture_src,
+                   &debug_texture_dest);
     SDL_RenderPresent(renderer);
+    // lets fuck with the text
+
+    load_debug_text();
   }
   return 0;
 }
