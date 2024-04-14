27f9249a77254bbcdaf6fec29b4e49943ef41bbb
darkmage <165251+mikedesu@users.noreply.github.com>
2024-04-14 14:01:59 -0500

    using CMake, moved files to src and include

--- update_loc.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-#
-a=$(grep -hv "^//" *.cpp | grep -v "^$" | wc -l); 
-b=$(grep -hv "^//" *.h | grep -v "^$" | wc -l); c=$((a+b)); 
-echo "$(date +'%Y-%m-%d %H:%M:%S'),$c" >> loc.csv
