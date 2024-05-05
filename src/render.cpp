bfa54856c1d417b795a2e17b6b68c646012ec453
darkmage <165251+mikedesu@users.noreply.github.com>
2024-05-05 00:51:16 -0500

    opportunities for inlining

--- src/render.cpp
+++ src/render.cpp
@@ -14,16 +14,14 @@ using std::map;
 using std::string;
 using std::unordered_map;
 
-extern int w, h;
+extern int w, h, frame_count;
 extern unordered_map<string, size_t> config;
 extern SDL_Renderer *renderer;
 extern map<entity_id, sprite_component> sprites;
-// extern unordered_map<entity_id, sprite_component> sprites;
 extern map<entity_id, sprite_component> bg_sprites;
 extern unordered_map<entity_id, transform_component> transforms;
 extern unordered_map<entity_id, transform_component> bg_transforms;
 extern unordered_map<entity_id, bool> is_flipped;
-extern unordered_map<entity_id, bool> is_damaged;
 extern SDL_Texture *debug_texture;
 extern SDL_Texture *gameover_texture;
 extern SDL_Texture *stopwatch_texture;
@@ -34,7 +32,6 @@ extern SDL_Rect debug_texture_dest;
 extern SDL_Rect target_texture_src;
 extern SDL_Rect target_texture_dest;
 extern bool do_render_debug_panel;
-extern int frame_count;
 extern SDL_Texture *target_texture;
 
 extern void load_debug_text();
@@ -43,20 +40,17 @@ extern void load_stopwatch_text();
 function<void(sprite_pair)> draw_sprite = [](const sprite_pair p) {
   const entity_id id = p.first;
   SDL_Texture *t = p.second.texture;
-
   if (p.second.dmg_frames > 0) {
     t = sprites[id].dmg_texture;
     sprites[id].dmg_frames--;
-    // is_damaged[id] = false;
   }
-
   // SDL_RenderCopyEx(renderer, p.second.texture, &p.second.src, &p.second.dest,
   SDL_RenderCopyEx(renderer, t, &p.second.src, &p.second.dest,
                    transforms[id].angle, NULL,
                    is_flipped[id] ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
 };
 
-void render_debug_panel() {
+inline void render_debug_panel() {
   SDL_Color color = {0, 0, 0, 128};
   SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
   SDL_RenderFillRect(renderer, &debug_texture_dest);
@@ -64,16 +58,16 @@ void render_debug_panel() {
                  &debug_texture_dest);
 }
 
-void render_stopwatch() {
-  // SDL_Color color = {0, 0, 0, 255};
-  // SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-  // SDL_RenderFillRect(renderer, &debug_texture_dest);
+inline void render_stopwatch() {
   SDL_RenderCopy(renderer, stopwatch_texture, &stopwatch_texture_src,
                  &stopwatch_texture_dest);
 }
 
-void render_sprites() { for_each(sprites.begin(), sprites.end(), draw_sprite); }
-void render_bg_sprites() {
+inline void render_sprites() {
+  for_each(sprites.begin(), sprites.end(), draw_sprite);
+}
+
+inline void render_bg_sprites() {
   for_each(bg_sprites.rbegin(), bg_sprites.rend(), draw_sprite);
 }
 
@@ -81,43 +75,34 @@ void render_gameover() {
   SDL_SetRenderTarget(renderer, target_texture);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
-
   SDL_QueryTexture(gameover_texture, NULL, NULL, &w, &h);
   const int x = target_texture_dest.w / 4;
   const int y = 0;
   SDL_Rect src = {0, 0, w, h};
   SDL_Rect dest = {x, y, w, h};
-
   SDL_RenderCopy(renderer, gameover_texture, &src, &dest);
   SDL_SetRenderTarget(renderer, NULL);
   SDL_RenderCopy(renderer, target_texture, &target_texture_src,
                  &target_texture_dest);
-
   SDL_RenderPresent(renderer);
   frame_count++;
 }
 
 void render_frame() {
-  // clear the target texture
   SDL_SetRenderTarget(renderer, target_texture);
-  // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  //  SDL_SetRenderDrawColor(renderer, 0x66, 0x66, 0x66, 255);
   SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
   SDL_RenderClear(renderer);
   render_bg_sprites();
   render_sprites();
-  //  reset the render target
   SDL_SetRenderTarget(renderer, NULL);
   SDL_RenderCopy(renderer, target_texture, &target_texture_src,
                  &target_texture_dest);
   render_stopwatch();
   load_stopwatch_text();
-  //  render debug text
   if (do_render_debug_panel) {
     render_debug_panel();
     load_debug_text();
   }
-
   SDL_RenderPresent(renderer);
   frame_count++;
 }
